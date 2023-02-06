b04d1747d8dbd79d966f8fd00a186b1e5f960417
JVGC <joaovictorgarciacoelho@gmail.com>
2023-02-06 08:57:29 -0300

    Setting google blue color on tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        googleBlue: '#4285F4'
+      }
+    },
   },
   plugins: [],
 };
